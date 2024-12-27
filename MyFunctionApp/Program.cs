b5693be7c9bb648d94bd967c387d80a5452539f1
Radu Florescu <radu.florescu@hotmail.com>
2024-12-27 15:56:24 +0200

    cleanup in folder structure.

--- MyFunctionApp/Program.cs
+++ MyFunctionApp/Program.cs
@@ -1,18 +1,39 @@
-﻿using System;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using MyFunctionApp.Helpers;
 
-namespace MyFunctionApp
-{
-    internal class Program
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureFunctionsWorkerDefaults((IFunctionsWorkerApplicationBuilder
+        workerOptions) =>
     {
-        static void Main(string[] args)
-        {
-            // Build and run the host
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults() // No need to add timers explicitly
-                .Build();
+        //workerOptions.AddServiceBus();
+        //workerOptions.AddTimers();
+    })
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+    })
+    .ConfigureServices((context, services) =>
+    {
+        // Get Key Vault URI
+        string keyVaultUri = context.Configuration["KeyVaultUri"];
 
-            host.Run();
-        }
-    }
-}
+        // Register KeyVaultHelper
+        var keyVaultHelper = new KeyVaultHelper(keyVaultUri);
+        services.AddSingleton(keyVaultHelper);
+
+        // Register BlobServiceClient
+        string blobConnectionString = keyVaultHelper.GetSecretAsync("BlobStorageConnectionString").Result;
+        services.AddSingleton(new BlobServiceClient(blobConnectionString));
+
+        // Register Func<string, Task<string>> for lazy secret retrieval
+        services.AddSingleton<Func<string, Task<string>>>(keyVaultHelper.GetSecretAsync);
+    })
+    .Build();
+
+host.Run();
